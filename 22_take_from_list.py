c69efabe303f9069b39680118ae24edf901d89dd
ken <mafiahsu0.1@gmail.com>
2024-07-07 15:05:29 +0800

    FEAT: 增加list教學範例檔

---
+++ 22_take_from_list.py
@@ -0,0 +1,13 @@
+# 創建一個列表
+fruits = ["apple", "banana", "cherry"]
+
+# 訪問第一個元素
+print(fruits[0])  # 輸出: apple
+
+# 訪問最後一個元素
+print(fruits[-1])  # 輸出: cherry
+
+
+# 搭配for迴圈取出所有元素
+for frunit in fruits:
+    print(frunit)
---
+++ 23_add_element_to_list.py
@@ -0,0 +1,10 @@
+# 創建一個列表
+fruits = ["apple", "banana", "cherry"]
+
+# 使用 append() 方法在列表末尾添加元素
+fruits.append("orange")
+print(fruits)  # 輸出: ['apple', 'banana', 'cherry', 'orange']
+
+# 使用 insert() 方法在指定位置插入元素
+fruits.insert(1, "kiwi")
+print(fruits)  # 輸出: ['apple', 'kiwi', 'banana', 'cherry', 'orange']
